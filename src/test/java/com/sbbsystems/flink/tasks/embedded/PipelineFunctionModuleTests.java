9ed4f22b997858eb953040856eb6111dd0c18794
Frans King <frans.king@sbbsystems.com>
2023-04-17 11:33:26 +0100

    Project skeleton

---
+++ src/test/java/com/sbbsystems/flink/tasks/embedded/PipelineFunctionModuleTests.java
@@ -0,0 +1,12 @@
+package com.sbbsystems.flink.tasks.embedded;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class PipelineFunctionModuleTests {
+    @Test
+    void addition() {
+        assertEquals(2, 2);
+    }
+}
