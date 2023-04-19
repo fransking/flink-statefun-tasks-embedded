29b8c4b96b005757488d9686e3f7eca4089093d3
Frans King <frans.king@sbbsystems.com>
2023-04-19 21:59:28 +0100

    Project skeleton - restructure into modules

---
+++ flink-statefun-tasks-embedded-test-harness/src/test/java/com/sbbsystems/flink/tasks/embedded/PipelineFunctionModuleHarnessTests.java
@@ -0,0 +1,48 @@
+package com.sbbsystems.flink.tasks.embedded;
+
+import com.google.protobuf.Any;
+import com.google.protobuf.StringValue;
+import com.sbbsystems.flink.tasks.embedded.generated.TaskRequest;
+import com.sbbsystems.flink.tasks.embedded.utils.IngressMessageSupplier;
+import org.apache.flink.statefun.flink.harness.Harness;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.stream.Collectors;
+
+import static com.sbbsystems.flink.tasks.embedded.testmodule.IoIdentifiers.REQUEST_INGRESS;
+import static com.sbbsystems.flink.tasks.embedded.testmodule.IoIdentifiers.RESULT_EGRESS;
+import static com.sbbsystems.flink.tasks.embedded.utils.HarnessUtils.startHarnessInTheBackground;
+import static org.apache.flink.util.function.FunctionUtils.uncheckedFunction;
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+public class PipelineFunctionModuleHarnessTests {
+    static LinkedBlockingDeque<Any> egressMessages = new LinkedBlockingDeque<>();
+
+    @Test
+    void testSendingMessages() throws Exception {
+        var ingress = IngressMessageSupplier.create(List.of(
+                TaskRequest.newBuilder().setRequest(Any.pack(StringValue.of("hello"))).build(),
+                TaskRequest.newBuilder().setRequest(Any.pack(StringValue.of("world"))).build()
+        ));
+        var harness = new Harness()
+                .withParallelism(1)
+                .withSupplyingIngress(REQUEST_INGRESS, ingress)
+                .withConsumingEgress(RESULT_EGRESS, msg -> egressMessages.add(msg));
+
+        List<Any> result = new ArrayList<>();
+        try (AutoCloseable ignored = startHarnessInTheBackground(harness)) {
+            result.add(egressMessages.take());
+            result.add(egressMessages.take());
+        }
+
+        var resultStrings = result.stream()
+                .map(uncheckedFunction((Any any) -> any.unpack(StringValue.class).getValue()))
+                .collect(Collectors.toList());
+
+        assertThat(resultStrings).containsExactlyInAnyOrder("h", "w");
+    }
+}
---
+++ flink-statefun-tasks-embedded/src/test/java/com/sbbsystems/flink/tasks/embedded/types/MessageTypesTests.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright [2023] [Frans King, Luke Ashworth]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.sbbsystems.flink.tasks.embedded.types;
+
+import com.sbbsystems.flink.tasks.embedded.generated.TaskException;
+import com.sbbsystems.flink.tasks.embedded.generated.TaskRequest;
+import com.sbbsystems.flink.tasks.embedded.generated.TaskResult;
+import org.apache.flink.statefun.sdk.reqreply.generated.TypedValue;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+public class MessageTypesTests {
+    @Test
+    public void isType_returns_correct_type_for_task_request() {
+        var typedValue = TypedValue.newBuilder().setTypename("io.statefun_tasks.types/statefun_tasks.TaskRequest").build();
+        assertTrue(MessageTypes.isType(typedValue, TaskRequest.class));
+    }
+
+    @Test
+    public void isType_returns_correct_type_for_task_result() {
+        var typedValue = TypedValue.newBuilder().setTypename("io.statefun_tasks.types/statefun_tasks.TaskResult").build();
+        assertTrue(MessageTypes.isType(typedValue, TaskResult.class));
+    }
+
+    @Test
+    public void isType_returns_correct_type_for_task_exception() {
+        var typedValue = TypedValue.newBuilder().setTypename("io.statefun_tasks.types/statefun_tasks.TaskException").build();
+        assertTrue(MessageTypes.isType(typedValue, TaskException.class));
+    }
+
+    @Test
+    public void asType_returns_correct_type_for_task_request() throws InvalidMessageTypeException {
+        var typedValue = TypedValue.newBuilder().setTypename("io.statefun_tasks.types/statefun_tasks.TaskRequest").build();
+        assertInstanceOf(TaskRequest.class, MessageTypes.asType(typedValue, TaskRequest::parseFrom));
+    }
+
+    @Test
+    public void asType_returns_correct_type_for_task_result() throws InvalidMessageTypeException {
+        var typedValue = TypedValue.newBuilder().setTypename("io.statefun_tasks.types/statefun_tasks.TaskResult").build();
+        assertInstanceOf(TaskResult.class, MessageTypes.asType(typedValue, TaskResult::parseFrom));
+    }
+
+    @Test
+    public void asType_returns_correct_type_for_task_exception() throws InvalidMessageTypeException {
+        var typedValue = TypedValue.newBuilder().setTypename("io.statefun_tasks.types/statefun_tasks.TaskException").build();
+        assertInstanceOf(TaskException.class, MessageTypes.asType(typedValue, TaskException::parseFrom));
+    }
+}
