edb624beb4d689d824da05a60550596c48fd8164
Frans King <frans.king@sbbsystems.com>
2023-04-19 15:50:27 +0100

    Project skeleton - proving end ingress to egress works

--- src/main/java/com/sbbsystems/flink/tasks/embedded/PipelineFunction.java
+++ src/main/java/com/sbbsystems/flink/tasks/embedded/PipelineFunction.java
@@ -15,25 +15,50 @@
  */
 package com.sbbsystems.flink.tasks.embedded;
 
+import com.google.protobuf.Any;
 import com.sbbsystems.flink.tasks.embedded.proto.TaskRequest;
+import com.sbbsystems.flink.tasks.embedded.proto.TaskResult;
 import com.sbbsystems.flink.tasks.embedded.types.InvalidMessageTypeException;
 import com.sbbsystems.flink.tasks.embedded.types.MessageTypes;
 import org.apache.flink.statefun.sdk.Context;
 import org.apache.flink.statefun.sdk.StatefulFunction;
+import org.apache.flink.statefun.sdk.io.EgressIdentifier;
+import org.apache.flink.statefun.sdk.reqreply.generated.TypedValue;
+import org.apache.flink.statefun.sdk.egress.generated.KafkaProducerRecord;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class PipelineFunction implements StatefulFunction {
-    private static final Logger LOG = LoggerFactory.getLogger(PipelineFunctionModule.class);
+    private static final Logger LOG = LoggerFactory.getLogger(PipelineFunction.class);
 
     @Override
     public void invoke(Context context, Object input) {
         try {
             if (MessageTypes.isType(input, TaskRequest.class)) {
-                LOG.info("GOT A TASK_REQUEST_TYPE");
+                LOG.info("GOT A TASK_REQUEST");
 
                 var taskRequest = MessageTypes.asType(input, TaskRequest::parseFrom);
                 LOG.info("TASK_REQUEST_ID is {}", taskRequest.getId());
+
+                var egress = new EgressIdentifier<>("example", "kafka-generic-egress", TypedValue.class);
+                var taskResult = TaskResult.newBuilder()
+                        .setId(taskRequest.getId())
+                        .setUid(taskRequest.getUid())
+                        .build();
+
+                var egressRecord = KafkaProducerRecord.newBuilder()
+                        .setTopic(taskRequest.getReplyTopic())
+                        .setValueBytes(Any.pack(taskResult).toByteString())
+                        .build();
+
+                var typedValue = TypedValue.newBuilder()
+                        .setValue(egressRecord.toByteString())
+                        .setHasValue(true)
+                        .setTypename("type.googleapis.com/io.statefun.sdk.egress.KafkaProducerRecord")
+                        .build();
+
+                context.send(egress, typedValue);
             }
         }
         catch (InvalidMessageTypeException e) {
