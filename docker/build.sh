6d7ed9ffffe1a79aa2972c415326618faf146d0a
Frans King <frans.king@sbbsystems.com>
2023-08-08 14:30:29 +0100

    1.1-SNAPSHOT

--- docker/build.sh
+++ docker/build.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker build -t fransking/flink-statefun-tasks:3.2.0_1.0.0-SNAPSHOT -f docker/Dockerfile ..
+docker build -t fransking/flink-statefun-tasks:3.2.0_1.1-SNAPSHOT -f docker/Dockerfile ..
