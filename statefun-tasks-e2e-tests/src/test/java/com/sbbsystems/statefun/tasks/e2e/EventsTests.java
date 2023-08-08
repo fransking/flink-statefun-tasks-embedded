48e690053e168570d54f8b160653cd97000206f8
Frans King <frans.king@sbbsystems.com>
2023-08-08 19:48:52 +0100

    1.0-rc2: Including graph structure in pipeline created event

--- statefun-tasks-e2e-tests/src/test/java/com/sbbsystems/statefun/tasks/e2e/EventsTests.java
+++ statefun-tasks-e2e-tests/src/test/java/com/sbbsystems/statefun/tasks/e2e/EventsTests.java
@@ -17,6 +17,7 @@ package com.sbbsystems.statefun.tasks.e2e;
 
 import com.google.protobuf.Int32Value;
 import com.google.protobuf.InvalidProtocolBufferException;
+import com.google.protobuf.StringValue;
 import com.sbbsystems.statefun.tasks.generated.Event;
 import com.sbbsystems.statefun.tasks.generated.TaskResult;
 import com.sbbsystems.statefun.tasks.generated.TaskStatus;
@@ -24,8 +25,10 @@ import com.sbbsystems.statefun.tasks.utils.NamespacedTestHarness;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
 import java.util.stream.Collectors;
 
+import static com.sbbsystems.statefun.tasks.e2e.PipelineBuilder.inParallel;
 import static org.assertj.core.api.Assertions.assertThat;
 
 
@@ -39,16 +42,35 @@ public class EventsTests {
 
     @Test
     void test_pipeline_created_event_is_sent() throws InvalidProtocolBufferException {
-        var pipeline = PipelineBuilder
-                .beginWith("echo", Int32Value.of(1))
+//        var pipeline = PipelineBuilder
+//                .beginWith("echo", Int32Value.of(1))
+//                .continueWith("echo")
+//                .build();
+
+        var p1 = PipelineBuilder
+                .beginWith("echo", StringValue.of("a"))
                 .continueWith("echo")
                 .build();
+
+        var p2 = PipelineBuilder
+                .beginWith("echo", StringValue.of("b"))
+                .build();
+
+        var pipeline = inParallel(List.of(p1, p2)).build();
 
         var response = harness.runPipelineAndGetResponse(pipeline);
         var taskResult = response.unpack(TaskResult.class);
 
         var events = harness.getEvents(taskResult.getId());
-        assertThat(events.stream().filter(Event::hasPipelineCreated)).hasSize(1);
+        var pipelineCreatedEvents = events.stream().filter(Event::hasPipelineCreated).collect(Collectors.toUnmodifiableList());
+        assertThat(pipelineCreatedEvents).hasSize(1);
+
+        var event = pipelineCreatedEvents.get(0).getPipelineCreated();
+        assertThat(event.getPipeline().getEntriesCount()).isEqualTo(1);
+        assertThat(event.getPipeline().getEntries(0).hasGroupEntry()).isTrue();
+        assertThat(event.getPipeline().getEntries(0).getGroupEntry().getGroupCount()).isEqualTo(2);
+        assertThat(event.getPipeline().getEntries(0).getGroupEntry().getGroup(0).getEntriesCount()).isEqualTo(2);
+        assertThat(event.getPipeline().getEntries(0).getGroupEntry().getGroup(1).getEntriesCount()).isEqualTo(1);
     }
 
     @Test
