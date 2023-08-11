2c31299e7f73fadda2f5587e20b95a4a62f53085
Frans King <frans.king@sbbsystems.com>
2023-08-11 20:15:27 +0100

    Support non-legacy module.yaml file format #60

--- statefun-tasks-e2e-tests/src/main/java/com/sbbsystems/statefun/tasks/e2e/EndToEndFunctionModule.java
+++ statefun-tasks-e2e-tests/src/main/java/com/sbbsystems/statefun/tasks/e2e/EndToEndFunctionModule.java
@@ -15,7 +15,7 @@
  */
 package com.sbbsystems.statefun.tasks.e2e;
 
-import com.sbbsystems.statefun.tasks.PipelineFunctionModule;
+import com.sbbsystems.statefun.tasks.configuration.PipelineBinderV1;
 import com.sbbsystems.statefun.tasks.configuration.PipelineConfiguration;
 import com.sbbsystems.statefun.tasks.testmodule.IoIdentifiers;
 import org.apache.flink.statefun.sdk.spi.StatefulFunctionModule;
@@ -25,8 +25,6 @@ import java.util.Map;
 
 public class EndToEndFunctionModule implements StatefulFunctionModule {
     public void configure(Map<String, String> globalConfiguration, Binder binder) {
-        var mainModule = new PipelineFunctionModule();
-
         var configuration = PipelineConfiguration.of(
                 "e2e/embedded_pipeline",
                 "example/kafka-generic-egress",
@@ -34,7 +32,7 @@ public class EndToEndFunctionModule implements StatefulFunctionModule {
                 IoIdentifiers.EVENTS_TOPIC,
                 null);
 
-        mainModule.configure(configuration, binder);
+        PipelineBinderV1.INSTANCE.bind(configuration, binder);
         binder.bindFunctionProvider(EndToEndRemoteFunction.FUNCTION_TYPE, unused -> new EndToEndRemoteFunction());
     }
 }
