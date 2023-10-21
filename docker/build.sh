c0644882f7304fff2f04f1acb4ff0df1a5eef32c
Frans King <frans.king@sbbsystems.com>
2023-10-21 13:25:33 +0100

    1.2.0 release

--- docker/build.sh
+++ docker/build.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker build -t fransking/flink-statefun-tasks:3.3.0_1.2-SNAPSHOT -f docker/Dockerfile ..
+docker build -t fransking/flink-statefun-tasks:3.3.0_1.2.0 -f docker/Dockerfile ..
